17c03ec5f2a048243b3871175bf44be70479d444
sarahannnicholson <sarah.ann.nicholson@gmail.com>
2018-06-03 15:09:03 -0700

    added ground to model

--- src/components/cube.js
+++ src/components/cube.js
@@ -10,6 +10,7 @@ class Cube extends Component {
         super(props);
     }
     componentDidMount() {
+        
         var container, stats;
         var camera, scene, renderer;
         var mesh, geometry;
@@ -19,7 +20,7 @@ class Cube extends Component {
         animate();
         
         function init() {
-            container = document.getElementById( 'cubeCanvas' );
+            container = document.getElementById( 'characterCanvas' );
 
             // Camera
             camera = new THREE.PerspectiveCamera( 50, window.innerWidth / window.innerHeight, 1, 5000 );
@@ -34,39 +35,34 @@ class Cube extends Component {
             
             // Scene
             var background = new THREE.CubeTextureLoader()
-            .setPath( './scenes/' )
-            .load( [ 'dark-s_px.jpg', 'dark-s_nx.jpg', 'dark-s_py.jpg', 'dark-s_ny.jpg', 'dark-s_pz.jpg', 'dark-s_nz.jpg' ] );
-            
+                .setPath( './scenes/' )
+                .load( [ 'dark-s_px.jpg', 'dark-s_nx.jpg', 'dark-s_py.jpg', 'dark-s_ny.jpg', 'dark-s_pz.jpg', 'dark-s_nz.jpg' ] );        
             background.format = THREE.RGBFormat;
 
             scene = new THREE.Scene();
             scene.background = background;
+
+            // Ground 
+            var geometry = new THREE.CircleGeometry( 500, 32 );
+            var material = new THREE.MeshBasicMaterial( { color: 0x808080 } );
+            var circle = new THREE.Mesh( geometry, material );
+            circle.position.set( 0, -500, 0 );
+            circle.rotation.x = - Math.PI / 2;
+            scene.add( circle )
 
             // Lights
             var ambient = new THREE.AmbientLight( 0xffffff );
             scene.add( ambient );
             pointLight = new THREE.PointLight( 0xffffff, 2 );
             scene.add( pointLight );
-            
-            // Materials
-            var cubeMaterial3 = new THREE.MeshLambertMaterial( { color: 0xff6600, combine: THREE.MixOperation, reflectivity: 0.3 } );
-            var cubeMaterial2 = new THREE.MeshLambertMaterial( { color: 0xffee00} );
-            var cubeMaterial1 = new THREE.MeshLambertMaterial( { color: 0xffffff} );
-            
+                      
             // Models
             var objLoader = new THREE.OBJLoader();
             objLoader.load( './models/female02.obj', function ( object ) {
-                var head = object;
-                head.scale.multiplyScalar( 8 );
-                head.position.y = -500;
-                //head.material = cubeMaterial1;
-                var head2 = head.clone();
-                head2.position.x = - 900;
-                //head2.material = cubeMaterial2;
-                var head3 = head.clone();
-                head3.position.x = 900;
-                //head3.material = cubeMaterial3;
-                scene.add( head, head2, head3 )
+                var character = object;
+                character.scale.multiplyScalar( 8 );
+                character.position.y = -500;
+                scene.add( character )
             } );
             
             // Renderer
@@ -97,8 +93,8 @@ class Cube extends Component {
     render() {
         return (
             <div>
-                <h2>Cube</h2>
-                <div id="cubeCanvas"></div>
+                <h2>Character</h2>
+                <div id="characterCanvas"></div>
                 <p id='feedback'></p>
             </div>
         );
